61b54d9d73d0dd8d8553313d70a3a4ed9e65abc3
"Valentine Ifedayo" <"vcaleb01@gmail.com">
2022-03-12 23:30:59 +0100

    deployed contract with greetings of hello world

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+
+    string greeting;
+    constructor(string memory _greeting){
+        greeting = _greeting;
+    }
+
+    function setGreeting (string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function getGreeting () public view returns(string memory){
+        return greeting;
+    }
+}
